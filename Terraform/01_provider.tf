6374a2fddde82f1b91d735571f2cdd0538c3d7b8
Pragnesh <54304326+Pragnesh8900@users.noreply.github.com>
2024-03-15 13:44:53 +0530

    Update 01_provider.tf

--- Terraform/01_provider.tf
+++ Terraform/01_provider.tf
@@ -1,3 +1,7 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = var.host
+
+  client_certificate     = "var.client_cert"
+  client_key             = "var.client_key"
+  cluster_ca_certificate = "var.ca_cert"
 }
