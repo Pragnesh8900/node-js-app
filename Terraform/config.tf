e745baa5045f9124bbc569db3fc8e8875f47525b
Pragnesh <54304326+Pragnesh8900@users.noreply.github.com>
2024-03-15 14:00:29 +0530

    Create config.tf

---
+++ Terraform/config.tf
@@ -0,0 +1,52 @@
+resource "kubernetes_namespace" "test" {
+  metadata {
+    name = "nodejs"
+  }
+}
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nodejs"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "NodeJS"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "NodeJS"
+        }
+      }
+      spec {
+        container {
+          image = "nodejs"
+          name  = "nodejs-container"
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "test" {
+  metadata {
+    name      = "nodejs"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "NodePort"
+    port {
+      node_port   = 30201
+      port        = 3000
+      target_port = 3000
+    }
+  }
+}
